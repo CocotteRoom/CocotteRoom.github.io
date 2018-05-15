70fa4c27e4a6a18c99f093a74835675b2b0c253c
mfrachet <marvin.frachet@zenika.com>
2018-05-15 07:54:52 +0200

    feat(router): add Not Found route

--- src/pages/mainRouter/MainRouter.js
+++ src/pages/mainRouter/MainRouter.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Home } from './../home/Home';
 import { Recipe } from './../recipe/Recipe';
+import { NotFound } from './../notFound/NotFound';
 
 export const MainRouter = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/recipe" component={Recipe} />
+      <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
 );
