0919d2b8a13f1259f2b957fdd33c65cc0d52b6f8
mfrachet <marvin.frachet@zenika.com>
2018-05-16 13:36:42 +0200

    refactor(move story inside components):

--- src/components/button/Button.story.js
+++ src/components/button/Button.story.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { Button } from '../button/Button';
+import { Button } from './Button';
 
 storiesOf('Button', module).add('basic button', () => [<Button content="Click me !" />]);
