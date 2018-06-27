000e55e68b3ce14c7ab354b33af04e66f13a7637
Laetitia <laetitia@mbp-de-laetitia.home>
2018-06-27 22:33:41 +0200

    feat(ui): templating

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,3 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Header } from '../../template/header/Header';
 
-export const Home = () => <div>Hello world</div>;
+const Wrapper = styled.div``;
+
+const Main = styled.main`
+  padding-left: 200px;
+
+  background-color: pink;
+`;
+
+export const Home = () => (
+  <Wrapper>
+    <Header>
+      <input />
+    </Header>
+    <Main>
+      <div>Hello world</div>
+    </Main>
+  </Wrapper>
+);
--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,15 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Google, Facebook } from '../../app/authentication/providers';
+import { Header } from '../../template/header/Header';
+
+const Wrapper = styled.div``;
+
+const Main = styled.main`
+  padding-left: 200px;
+
+  background-color: pink;
+`;
 
 export const Login = () => (
-  <div>
-    <Google
-      onUserConnected={userInfo => console.log(userInfo)}
-      SigninButton={() => <div>Google</div>}
-    />
-    <Facebook
-      onUserConnected={userInfo => console.log(userInfo)}
-      SigninButton={() => <div>Facebook</div>}
-    />
-  </div>
+  <Wrapper>
+    <Header>
+      <div>
+        <Google
+          onUserConnected={userInfo => console.log(userInfo)}
+          SigninButton={() => <div>Google</div>}
+        />
+        <Facebook
+          onUserConnected={userInfo => console.log(userInfo)}
+          SigninButton={() => <div>Facebook</div>}
+        />
+      </div>
+    </Header>
+    <Main />
+  </Wrapper>
 );
