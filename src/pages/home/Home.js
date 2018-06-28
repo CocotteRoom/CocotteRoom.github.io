2fbc642a47e89fc0e88ef42984a4f591e895c7c6
Laetitia Bonanni <lbonanni.externe@m6.fr>
2018-06-28 13:36:51 +0200

    add main

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Wrapper from '../../components/wrapper/Wrapper';
 
 export const Home = () => (
-  <Wrapper marginLeft="200px" backgroundColor="pink" >
+  <Wrapper marginLeft="200px" backgroundColor="pink">
     <div>Hello world</div>
   </Wrapper>
 );
