be343cdc06ae2f36c47be8422907809642cda205
Laetitia Bonanni <lbonanni.externe@m6.fr>
2018-06-28 13:09:26 +0200

    Marvin review

--- src/components/wrapper/FixedWrapper.js
+++ src/components/wrapper/FixedWrapper.js
@@ -1,8 +1,8 @@
 import Wrapper from './Wrapper';
 
 export default Wrapper.extend`
-    position: fixed;
-    left: 0;
-    top: 0;
-    bottom: 0;
+  position: fixed;
+  left: 0;
+  top: 0;
+  bottom: 0;
 `;
